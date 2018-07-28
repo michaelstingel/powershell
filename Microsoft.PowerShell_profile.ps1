79172d34af630b10a7fc44e22ec9b6e491f6232d
Michael Stingel <michael.stingel@outlook.com>
2018-07-28 19:03:26 +0200

    Adds function to create Temp-Folder

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -4,4 +4,9 @@
 Set-Alias -Name tc -Value test-netconnection
 Set-Alias -Name ct -Value Connect-MicrosoftTeams
 Set-Alias -Name cad -Value Connect-AzureAD
-#
+
+function createTemp {
+    New-Item -ItemType Directory -Path C:\ -Name "Temp" -Force
+}
+
+createTemp
